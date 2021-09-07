6bf59082b0eceafdec02364b6650270a89eb655f
Akash Rupapara <rupaparaakash@Akashs-MacBook-Air.local>
2021-09-07 11:09:45 -0700

    Updates

---
+++ precache-manifest.7c78b4bb28ced79d0d9a98461f5c8f57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "25652158611f37a839f2",
+    "url": "/static/css/main.b9263a5a.chunk.css"
+  },
+  {
+    "revision": "25652158611f37a839f2",
+    "url": "/static/js/main.557ce476.chunk.js"
+  },
+  {
+    "revision": "28e856ba8edc36f0a860",
+    "url": "/static/js/runtime~main.7aeef26c.js"
+  },
+  {
+    "revision": "f10a4dfecf26ce947cbf",
+    "url": "/static/css/2.498bf8fe.chunk.css"
+  },
+  {
+    "revision": "f10a4dfecf26ce947cbf",
+    "url": "/static/js/2.802ec865.chunk.js"
+  },
+  {
+    "revision": "af1a9508fa92b22940d6",
+    "url": "/static/js/3.f9301ae8.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "a3f47aa65ee13f9fbdad08e56cc3526c",
+    "url": "/index.html"
+  }
+];
